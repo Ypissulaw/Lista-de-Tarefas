a363061ad089eb2f12d2c41aad537eb55790162e
Yasmin Miranda Godoy <129447508+Ypissulaw@users.noreply.github.com>
2025-01-19 08:13:54 -0300

    Delete config directory
    sera organizado

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//conectando ao banco de dados.
-try{
-    $pdo=new PDO("mysql:dbname=listatodo;host=localhost","root","");
-}catch(PDOException $erro){
-    echo "Problema ao se conectar com o banco :c".$erro->getMessage();
-}
-
-?>
