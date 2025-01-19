cbf4ed97f03d95f849e1625708f6cfc92357dd72
Yasmin Miranda Godoy <129447508+Ypissulaw@users.noreply.github.com>
2025-01-19 08:16:56 -0300

    barra lateral organizada
    barra lateral adicionada em todas os arquivos, novos estilos e pastas corretas

---
+++ crud/add/recebeAdd.php
@@ -0,0 +1,18 @@
+<?php
+require("../../config/config.php");
+if(!empty($_POST['titulo'])){
+    $titulo=$_POST['titulo'];
+    $corpo=$_POST['corpo'];
+}
+
+$sql = "INSERT INTO tarefas (titulo,corpo) VALUES (:titulo,:corpo)";
+$sql = $pdo->prepare($sql);
+$sql->bindParam(":titulo",$titulo);
+$sql->bindParam(":corpo",$corpo);
+$sql->execute();
+
+header("Location: ../../index.php");
+exit();
+
+
+?>
---
+++ crud/edit/recebeEdit.php
@@ -0,0 +1,19 @@
+<?php
+require("../config/config.php");
+if(isset($_POST['id'])){
+    $id=$_POST['id'];
+    $titulo=$_POST['titulo'];
+    $corpo=$_POST['corpo'];
+
+    $sql="UPDATE teste SET titulo=:titulo, corpo=:corpo WHERE id=:id";
+    $sql=$pdo->prepare($sql);
+    $sql->bindParam(":id",$id);
+    $sql->bindParam(":titulo",$titulo);
+    $sql->bindParam( ":corpo",$corpo);
+    $sql->execute();
+    header("Location: ../../index.php");
+    exit();
+    
+}
+
+?>
